fbf7adfc37b5a51d986937d36861bd166c27faba
Maxime Carlier <maxime.mca@gmail.com>
2018-02-28 00:50:59 +0100

    Fix bad dependency

--- sensilang-api/src/main/java/fr/unice/polytech/dsl/sensilang/model/sensor/mutators/Mutator.java
+++ sensilang-api/src/main/java/fr/unice/polytech/dsl/sensilang/model/sensor/mutators/Mutator.java
@@ -1,7 +1,5 @@
 package fr.unice.polytech.dsl.sensilang.model.sensor.mutators;
 
-import com.sun.istack.internal.Nullable;
-
 public interface Mutator {
-    Number mutate(@Nullable Number n);
+    Number mutate(Number n);
 }
