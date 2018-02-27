37d27ac25538b0e31386271c6ebba9fcda2a716d
Maxime Carlier <maxime.mca@gmail.com>
2018-02-27 11:36:58 +0100

    Add Interpolation to java API

--- sensilang-api/src/main/java/fr/unice/polytech/dsl/sensilang/main/Main.java
+++ sensilang-api/src/main/java/fr/unice/polytech/dsl/sensilang/main/Main.java
@@ -3,19 +3,20 @@ package fr.unice.polytech.dsl.sensilang.main;
 import fr.unice.polytech.dsl.sensilang.model.sensor.*;
 import fr.unice.polytech.dsl.sensilang.model.sensor.mutators.Mutator;
 import fr.unice.polytech.dsl.sensilang.model.sensor.mutators.NoiseMutator;
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
+import org.apache.commons.math3.analysis.interpolation.UnivariateInterpolator;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractSensor s1 = new ReplaySensor<>("replay_sensor1", Integer.class)
-                .withLocalFile("sensilang-api/src/main/resources/Bike1.csv")
-                .asCrossedCsv(10);
+        double x[] = {0.0, 1.0, 2.0};
+        double y[] = {1.0, -1.0, 2.0};
 
-        Mutator noiseMutator = new NoiseMutator(0.1);
+        UnivariateInterpolator interpolator = new SplineInterpolator();
+        UnivariateFunction function = interpolator.interpolate(x, y);
 
-        s1.setMutator(noiseMutator);
 
-        for(int i=0; i<=1000; i+=100) {
-            System.out.println(s1.getFinalValue(i));
-        }
+
     }
 }
